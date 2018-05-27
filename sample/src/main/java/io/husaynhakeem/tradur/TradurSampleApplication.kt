56470a3b52f4c069630f00b2e1cb1b3151fe279d
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-05-27 00:59:12 -0400

    Move api key to keys.xml

--- sample/src/main/java/io/husaynhakeem/tradur/TradurSampleApplication.kt
+++ sample/src/main/java/io/husaynhakeem/tradur/TradurSampleApplication.kt
@@ -4,10 +4,10 @@ import android.app.Application
 import io.husaynhakeem.tradurlibrary.Tradur
 
 
-class TradurSampleApplication: Application() {
+class TradurSampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Tradur.init("AIzaSyCYf85QKk6x2ZbZroZRp_YemJaDT0gLpUQ")
+        Tradur.init(getString(R.string.google_translate_api_key))
     }
 }
